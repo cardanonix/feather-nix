78d155c949e09fb15e117f05cea74edbd312f57e
harryprayiv <harryprayiv@gmail.com>
2022-09-25 09:35:37 -0400

    shurgs more

--- system/machine/onyxTower/hardware-configuration.nix
+++ system/machine/onyxTower/hardware-configuration.nix
@@ -12,5 +12,5 @@
       fsType = "vfat";
     };
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  #hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
