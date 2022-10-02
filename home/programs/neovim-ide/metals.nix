897ba603ac3d178716ce2b442b6583ffe3c79f84
harryprayiv <harryprayiv@gmail.com>
2022-10-02 12:22:21 -0400

    update metals

--- home/programs/neovim-ide/metals.nix
+++ home/programs/neovim-ide/metals.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
 pkgs.metalsBuilder {
-  version = "0.11.8+157-e74c0082-SNAPSHOT";
-  outputHash = "sha256-kGSSdMR9Z6rE1u8S7lyfdy0YjFnWlCcP0WfLk4ZmKTw=";
+  version = "0.11.8+196-d47496ed-SNAPSHOT";
+  outputHash = "sha256-XQGoHxEeeFVv5L++rZSE286o+IlUeJWvJQwbPqA6cx4=";
 }
