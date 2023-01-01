84e5ca9435b0e4b61121f7d8e2602866b42d741a
harryprayiv <harryprayiv@gmail.com>
2023-01-01 15:01:33 -0500

    git worktree changes

--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -2,7 +2,7 @@
 
 let
   gitConfig = {
-    safe.directory = "/home/bismuth/$";
+    safe.directory = "*";
     #safe.directory = "/home/bismuth/Cardano/git/IOHK/Alonzo-testnet";
     #safe.directory = "/home/bismuth/Cardano/git/IOHK/";
     gpg.program = "${pkgs.gnupg}/bin/gpg2";
