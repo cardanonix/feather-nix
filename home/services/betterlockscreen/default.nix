f9999d8e5af7be5f73183304942284cd0861fbc5
harryprayiv <harryprayiv@gmail.com>
2023-01-20 21:38:10 -0500

    declarative config of betterlockscreen

--- home/services/betterlockscreen/default.nix
+++ home/services/betterlockscreen/default.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 
 {
-  xdg.configFile."i3blocks/config".source = ./betterlockscreenrc;
+  xdg.configFile."betterlockscreenrc".source = ./betterlockscreenrc;
   home.file.".betterlockscreenrc".text = ''
       set auto-load safe-path /nix/store
   '';
 }
-#This will create symlink $XDG_CONFIG_HOME/i3blocks/config and ~/.gdbinit
+#This will create symlink $XDG_CONFIG_HOME/betterlockscreen and ~/.betterlockscreenrc
 
