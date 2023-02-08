f01d58ddba7cc1c87081b1ea515ef8bae85e0ed6
harryprayiv <harryprayiv@gmail.com>
2023-02-08 00:30:24 -0500

    non-working demo of consolidated cardano settings

--- home/services/cardano-node/canonical-node-attempt.nix
+++ home/services/cardano-node/canonical-node-attempt.nix
@@ -11,16 +11,26 @@ let
 in
 {   
   environment.systemPackages = with pkgs; [
-      inputs.cardano-node.packages.x86_64-linux.cardano-node
-      inputs.cardano-node.packages.x86_64-linux.cardano-cli
+    pinentry-curses
+    bech32
+    cardano-node.packages.x86_64-linux.cardano-node
+    cardano-cli
+    sqlite-interactive
+    srm
   ];
-  users.groups.cardano-node.gid = 1002;
   
-  config.services.cardano-node = {
+  environment.variables = {
+    #CARDANO_NODE_SOCKET_PATH = config.services.cardano-node.socketPath;
+  };
+  
+  users.groups.cardano-node.gid = 1002;
+    
+  services.cardano-node = {
       enable = true;
-      package = inputs.cardano-node.packages.x86_64-linux.cardano-node;
+      package = pkgs.cardano-node;
       systemdSocketActivation = true;
       environment = "mainnet";
+      environments = cardano-node.environments.x86_64-linux;
       useNewTopology = true;
       topology = "${topology}";
       nodeConfigFile = "${home}${nodeconfig}";
@@ -29,5 +39,8 @@ in
       rtsArgs = [ "-N2" "-I0" "-A16m" "-qg" "-qb" "--disable-delayed-os-memory-return" ]; 
       #nodeId = "bismuthian Test!!!";
   };
+
+  systemd.sockets.cardano-node.partOf = [ "cardano-node.socket" ];
+  systemd.services.cardano-node.after = lib.mkForce [ "network-online.target" "cardano-node.socket" ];
 
 }
