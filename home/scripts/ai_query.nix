8d4010650728a92b25ef4bb1c15f8139c79dcad6
harryprayiv <harryprayiv@gmail.com>
2023-02-20 08:20:13 -0500

    more ai stuff

--- home/scripts/ai_query.nix
+++ home/scripts/ai_query.nix
@@ -1,20 +1,20 @@
 { config, pkgs, ... }:
 let
-  prompt   = "Write a beat poem about the complex relationship between knowing something and believing something in the style of William S. Burroughs.";
   api_key  = builtins.readFile /home/bismuth/plutus/Documents/Credentials/davinci_api_key.txt;
   curl     = "${pkgs.curl}/bin/curl";
   jq       = "${pkgs.jq}/bin/jq";
   sed      = "${pkgs.gnused}/bin/sed";
-  ponysay  = "${pkgs.ponysay}/bin/ponysay";
+  # ponysay  = "${pkgs.ponysay}/bin/ponysay";
 in
-pkgs.writeShellScriptBin "ai_completion" ''
-    ${curl} https://api.openai.com/v1/completions \
+pkgs.writeShellScriptBin "ai_query" ''
+    read -p "Enter a prompt: " prompt
+    ${curl} https://api.openai.com/v1/completions -s \
     -H 'Content-Type: application/json' \
     -H 'Authorization: Bearer ${api_key}' \
     -d '{
     "model": "text-davinci-003",
-    "prompt": "${prompt}",
+    "prompt": "'"$prompt"'",
     "max_tokens": 1000,
     "temperature": 0
-    }' | ${jq} '.choices' | ${jq} -r '.[0].text' | ${sed} 's/"//g' | ${ponysay}
+    }' | ${jq} '.choices' | ${jq} -r '.[0].text' | ${sed} 's/"//g'
   ''
--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,30 +1,35 @@
 let
   scripts = { config, pkgs, ... }:
     let
-      gen-ssh-key   = pkgs.callPackage ./gen-ssh-key.nix { inherit pkgs; };
-      hcr           = pkgs.callPackage ./changes-report.nix { inherit config pkgs; };
-      hms           = pkgs.callPackage ./switcher.nix { inherit config pkgs; };
-      kls           = pkgs.callPackage ./keyboard-layout-switch.nix { inherit pkgs; };
-      szp           = pkgs.callPackage ./show-zombie-parents.nix { inherit pkgs; };
-      node_toggle   = pkgs.callPackage ./node_toggle.nix { inherit config pkgs; };
-      node_check    = pkgs.callPackage ./node_check.nix { inherit config pkgs; };
-      hue_tools     = pkgs.callPackage ./hue_tools.nix { inherit pkgs; };
-      mru           = pkgs.callPackage ./mru.nix { inherit config pkgs; };
-      ai_completion = pkgs.callPackage ./ai_completion.nix { inherit config pkgs; };
+      gen-ssh-key         = pkgs.callPackage ./gen-ssh-key.nix { inherit pkgs; };
+      hcr                 = pkgs.callPackage ./changes-report.nix { inherit config pkgs; };
+      hms                 = pkgs.callPackage ./switcher.nix { inherit config pkgs; };
+      kls                 = pkgs.callPackage ./keyboard-layout-switch.nix { inherit pkgs; };
+      szp                 = pkgs.callPackage ./show-zombie-parents.nix { inherit pkgs; };
+      node_toggle         = pkgs.callPackage ./node_toggle.nix { inherit config pkgs; };
+      node_check          = pkgs.callPackage ./node_check.nix { inherit config pkgs; };
+      hue_tools           = pkgs.callPackage ./hue_tools.nix { inherit pkgs; };
+      mru                 = pkgs.callPackage ./mru.nix { inherit config pkgs; };
+      queryAI             = pkgs.callPackage ./ai_query.nix { inherit config pkgs; };
+      personAI_query      = pkgs.callPackage ./personAI/query.nix { inherit config pkgs; };
+      personAI_translate  = pkgs.callPackage ./personAI/translate.nix { inherit config pkgs; };
+    
     in
     {
       home.packages =
         [
-          hcr               # home-manager changes report between generations
-          gen-ssh-key       # generate ssh key and add it to the system
-          kls               # switch keyboard layout
-          szp               # show zombie parents
-          node_toggle       # launch cardano-node
-          node_check        # cardano node monitor script
-          hue_tools         # tools for interacting with hue lights
-          mru               # repo download
-          ai_completion      # repo download
-          #hms             # custom home-manager switcher that considers the current DISPLAY
+          hcr                # home-manager changes report between generations
+          gen-ssh-key        # generate ssh key and add it to the system
+          kls                # switch keyboard layout
+          szp                # show zombie parents
+          node_toggle        # launch cardano-node
+          node_check         # cardano node monitor script
+          hue_tools          # tools for interacting with hue lights
+          mru                # repo download
+          queryAI            # openAI chat prompt cli
+          personAI_query     # openAI chat prompt cli
+          personAI_translate # openAI chat prompt cli
+          #hms               # custom home-manager switcher that considers the current DISPLAY
           #ipad-mirror
         ];
     };
