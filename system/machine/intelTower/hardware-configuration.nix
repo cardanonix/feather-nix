8290236ccace02cb161cd501f74133b16884e309
harryprayiv <harryprayiv@gmail.com>
2023-05-11 14:03:45 -0400

    hardware changes (adding zfs pool)

--- system/machine/intelTower/hardware-configuration.nix
+++ system/machine/intelTower/hardware-configuration.nix
@@ -7,9 +7,16 @@
       device = "/dev/disk/by-uuid/E190-B72F";
       fsType = "vfat";
     };
+
   fileSystems."/" =
     { 
       device = "/dev/disk/by-uuid/b7936e98-ae0f-4c3d-99f1-c27b97968d58";
       fsType = "ext4";
+    };
+
+  fileSystems."/z_pool" =
+    { 
+      device = "/dev/disk/by-uuid/4210348493706516964";
+      fsType = "zfs";
     };
 }
