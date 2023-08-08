d1e6e6f2c6bc74b1db41c9c8c93630439dd1133b
harryprayiv <harryprayiv@gmail.com>
2023-08-07 23:57:53 -0400

    extraneuous file cleanup

--- system/machine/cardano.town/hm/base.nix
+++
@@ -1,6 +0,0 @@
-{...}: {
-  home.stateVersion = "23.05";
-  home.username = "bakerdn";
-  home.homeDirectory = "/home/bakerdn";
-  programs.home-manager.enable = true;
-}
--- system/machine/cardano.town/hm/neovim/coc.vim
+++
@@ -1,5 +0,0 @@
-" ------------------- COC config -----------------------
-
-" coc-explorer setup
-nmap <space>e <Cmd>CocCommand explorer<CR>
-
