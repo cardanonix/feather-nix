4051f9c937f4587b18752f40c8f77974684ab02e
harryprayiv <harryprayiv@gmail.com>
2023-08-09 17:04:25 -0400

    added feh

--- home/home.nix
+++ home/home.nix
@@ -68,6 +68,7 @@
     srm # a command-line compatible rm(1) which overwrites file contents before unlinking.
     pinentry # a small collection of dialog programs that allow GnuPG to read passphrases and PIN numbers in a secure manner
     spago
+    feh
 
     libsForQt5.gwenview # my favorite image viewer (for now)
 
