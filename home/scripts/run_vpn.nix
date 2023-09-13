9538e051a6a70f92c20d7322fb312c513c80c517
harryprayiv <harryprayiv@gmail.com>
2023-09-13 12:55:27 -0400

    secrets can bite you sometimes

--- home/scripts/run_vpn.nix
+++ home/scripts/run_vpn.nix
@@ -21,6 +21,11 @@ in
         echo Starting VPN...
         sudo ${systemctl} start "openvpn-${vpn_name}.service"
         # ${downloader}
-        echo IP Protected: VPN Started
+      if ${systemctl} is-active "openvpn-${vpn_name}.service" > /dev/null; then
+          echo ATTN: Your IP is now Hidden
+          echo ** VPN Connected to ${vpn_name} !!**
+      else
+          echo Your IP is Vulnerable: VPN Didn't Launch
+      fi
     fi
   ''
