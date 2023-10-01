947b8f409622b5ad0becce631882a8e4e27d8f00
harryprayiv <harryprayiv@gmail.com>
2023-10-01 13:22:14 -0400

    shell hook

--- shell.nix
+++ shell.nix
@@ -6,6 +6,14 @@ with pkgs;
     ];
 
     shellHook = ''
-      # ...
+      export NIX_SHELL_NAME="nix-config shell"
+      echo "Welcome to my nix-config devShell!"
+      echo
+      echo opening VSCodium for this project....
+      codium .
+      echo .
+      echo ..
+      echo welcome to the my devshell
+
     '';
   }
